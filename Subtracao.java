b2c6a7cce43bebc93c79091ef9d3baf37df7eab9
Jhon Med <jhonatan1medeiros@gmail.com>
2021-11-22 11:45:54 -0300

    Que surra do git

--- Subtracao.java
+++ Subtracao.java
@@ -45,7 +45,7 @@ public class Subtracao implements ICalcJava
        
        do
        {
-            System.out.println("insira um numero ou tecle '=' para fazer a SUBTRAÇÃO: ");
+            System.out.println("insira um numero ou tecle '=' para fazer a SUBTRAÇÃO!: ");
             x = kb.next();
             if(x.equals("="))
             {
