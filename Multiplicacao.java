dc768a435d89ac635cec81c335fb329a9be8d944
Yuxis445 <luffy2033@outlook.com>
2021-11-22 13:52:34 -0300

    bug corrigido

--- Multiplicacao.java
+++ Multiplicacao.java
@@ -4,29 +4,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Multiplicacao implements ICalcJava{
-    
+    Scanner kb = new Scanner(System.in);
     String x;
     double multTotal;
-    Scanner kb = new Scanner(System.in);
-    
+
     @Override
     public void fazerDivisao() {
         // TODO Auto-generated method stub
         
     }
+
     @Override
     public void fazerExponencial() {
         // TODO Auto-generated method stub
         
     }
+
     @Override
     public void fazerMultiplica() {
-
-        ArrayList<Double> mult = new ArrayList<>();
+        ArrayList<Double> multiplicacao = new ArrayList<>();
        
         do
         {
-             System.out.println("insira um numero ou tecle '=' para fazer a MULTIPLICAÇÃO!: ");
+             System.out.println("insira um numero ou tecle '=' para fazer a SUBTRAÇÃO!: ");
              x = kb.next();
              if(x.equals("="))
              {
@@ -34,40 +34,47 @@ public class Multiplicacao implements ICalcJava{
              }
              else
              {
-                 mult.add(Double.parseDouble(x));
+                 multiplicacao.add(Double.parseDouble(x));
              }
  
         }while(! x.equals("="));
         
-        double mult1 = mult.get(0);
-        double multiplicar=0;
-        for(int i = 1 ; i < mult.size() ; i++)
+        double mult1 = multiplicacao.get(0);
+        double multiplica=0;
+        for(int i = 1 ; i < multiplicacao.size() ; i++)
         {
-             multiplicar += mult.get(i);
+             multiplica += multiplicacao.get(i);
             
+         
         }
-
-        multTotal = (mult1) * (multiplicar);
-
+        if(mult1 < 0)
+        {
+             multTotal =  (multiplica) + (mult1);
         }
-        System.out.println("subtracao dos numeros :" + mult.toString());
+        else{
+             multTotal = (mult1) - (multiplica);
+        }
+        System.out.println("subtracao dos numeros :" + multiplicacao.toString());
         System.out.println("e igual a: " + multTotal);
         
     }
+
     @Override
     public void fazerRaiz() {
         // TODO Auto-generated method stub
         
     }
+
     @Override
     public void fazerSoma() {
         // TODO Auto-generated method stub
         
     }
+
     @Override
     public void fazerSubtracao() {
         // TODO Auto-generated method stub
         
-    } 
-
+    }
     
+}
